c5da598628f1fed9679b0ec311440679baac698a
Yacell Dhariana Borges Almonte <yborges@outcoding.com>
2022-04-26 09:24:27 -0400

    Adding constant path for getting elements

--- SurveySystem/Common/Constants/Constants.cs
+++ SurveySystem/Common/Constants/Constants.cs
@@ -8,5 +8,6 @@ namespace SurveySystem.Common.Constants
     public static class Constants
     {
         public const string ElementAnswerType = "A";
+        public const string GET_ELEMENTS_REST = "get-elements";
     }
 }
